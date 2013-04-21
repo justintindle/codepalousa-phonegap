789a6a22e70681b80f6675427dbea98633a8a3c5
Justin Tindle <justin.tindle@gmail.com>
2013-04-21 18:37:41 -0400

    Added WP8 demo and SPA/WP8 solution.

---
+++ SPA/Tindle.CodePaLOUsa.SPA/Controllers/AppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tindle.CodePaLOUsa.SPA.Controllers
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
